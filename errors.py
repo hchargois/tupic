70e5f14072bb61797048f50375e2738e9efadc87
Lertsenem <lertsenem@yahoo.fr>
2013-10-14 03:00:04 +0200

    Adding 'listshows' feature for 'arteplus7' source
    This allows to list the shows available on the server and print the
    result nicely, with infos and shit. Cool huh ?
    Next: put the Sources stuff in a separate sources.py file. But now I'm
    going to get some sleep.

--- errors.py
+++ errors.py
@@ -19,4 +19,14 @@ class UnsupportedSourceError(Exception):
         self.source = source
 
     def __str__(self):
-        return 'The source \'' + self.value + '\' you are trying to use is not supported yet'
+        return 'The source \'' + self.source + '\' you are trying to use is not supported yet'
+
+class UnsupportedFeatureError(Exception):
+	"""The Feature is not supported for the used Source"""
+	
+	def __init__(self, source, feature):
+		self.source = source
+		self.feature = feature
+
+	def __str__(self):
+		return 'The \'' + self.feature + '\' Feature is not available for the \'' + self.source +'\' Source'
--- features.py
+++ features.py
@@ -12,7 +12,7 @@
 
 # CLASSES
 ########################################
-
+# ======================================
 class SourceFeature(object):
 	"""An operation available for a Source"""
 
@@ -24,6 +24,7 @@ class SourceFeature(object):
 
 		raise NotImplementedError("Abstract, override it")
 
+# ======================================
 class GetfromplayerSourceFeature(SourceFeature):
 	"""Get the show from a given url containing the player."""
 	def __init__(self):
@@ -37,4 +38,35 @@ class GetfromplayerSourceFeature(SourceFeature):
 
 		return "", "", []
 	
+# ======================================
+class ListshowsSourceFeature(SourceFeature):
+	"""List the shows available for the Source."""
+	def __init__(self):
+		super(ListshowsSourceFeature, self).__init__('listshows')
+		
+
+	def do(self):
+		"""Should return a list of dictionnaries containing info on
+		each show.
+		Only the 'title' element of the dictionnary, which MUST
+		contain the title of the show, is mandatory.
+
+		The other possible elements are:
+
+		desc:			a short description of the show
+		url:			the show (player) URL
+		duration:		show duration, in minutes
+		airdate_long:		a string for the air date
+		video_rights_until:	a string indicating how much time is
+					left before the video disappear
+					from the server
+		video_views:		the number of views for the video
+		video_channels:		a string containing tags relative to
+					the show
+		video_rank:		the source-dependent site ranking of
+					the show
+
+		This is implemented on a per-instance basis"""
+
+		return []
 
